c0a9f6836473e0600a6fb9432f96bde7a46c1859
yornellas <yo_ornellas@hotmail.com>
2025-01-16 11:54:08 -0300

    feat: implemets conditional rendering on Footer

--- src/shared/components/Footer/index.jsx
+++ src/shared/components/Footer/index.jsx
@@ -1,9 +1,21 @@
 function Footer() {
-  return (
-    <footer>
-blabla
-    </footer>
-  )
+  const hour = new Date().getHours()
+  const openHour = 11
+  const closeHour = 23
+  const isOpen = hour >= openHour && hour <= closeHour
+
+  function setFooterInfo() {
+    return isOpen ? (
+      <div className="order">
+        We're open until {closeHour}:00. Order online!
+        <button className="btn">Order now</button>
+      </div>
+    ) : (
+      <div className="order">We open at {closeHour}:00.</div>
+    )
+  }
+
+  return <footer>{setFooterInfo()}</footer>
 }
 
 export default Footer
