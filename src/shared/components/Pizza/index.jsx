3af45cf278edbbc1700514d258910020773688ff
yornellas <yo_ornellas@hotmail.com>
2025-01-16 14:52:35 -0300

    feat: set conditional text and class

--- src/shared/components/Pizza/index.jsx
+++ src/shared/components/Pizza/index.jsx
@@ -1,11 +1,11 @@
 function Pizza({ pizzaObject }) {
   return (
-    <div className="pizza">
+    <div className={`pizza ${pizzaObject.soldOut ? 'sold-out' : ''}`}>
       <img src={pizzaObject.photo} alt={pizzaObject.name} />
       <div>
         <h3>{pizzaObject.name}</h3>
         <p>{pizzaObject.ingredients}</p>
-        <span>{pizzaObject.price}</span>
+        <span> {pizzaObject.soldOut ? 'SOLD OUT' : pizzaObject.price}</span>
       </div>
     </div>
   )
