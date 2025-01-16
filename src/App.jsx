4f55b0b3c1e6aa3ca0daf4d7e99d7de778873fb8
yornellas <yo_ornellas@hotmail.com>
2025-01-16 09:14:34 -0300

    feat: apply components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Footer from './shared/components/Footer'
+import Header from './shared/components/Header'
+import Menu from './shared/components/Menu'
 
 function App() {
   return (
-    <>
-    </>
+    <div className='container'>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
   )
 }
 
