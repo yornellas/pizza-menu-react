3215d934f492168c9e58a9bdf2ec5e20a0c6d633
yornellas <yo_ornellas@hotmail.com>
2025-01-16 09:13:52 -0300

    feat: create Pizza component

---
+++ src/shared/components/Pizza/index.jsx
@@ -0,0 +1,14 @@
+function Pizza({ props }) {
+  return (
+    <div className="pizza">
+      <img src={props.photo} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Pizza
